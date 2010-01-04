c4cce903d2067377e98789610b491d447c30d724
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2010-01-04 07:12:52 +0000

    ウィンドウのIDをきちんと取れていなかった
    git-svn-id: https://www.cozmixng.org/repos/piro/fx3-compatibility-lib/trunk@5710 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- operationHistory.test.js
+++ operationHistory.test.js
@@ -7,6 +7,7 @@ var windowSetUp = function() {
 };
 var windowTearDown = function() {
 	yield Do(utils.tearDownTestWindow());
+	win = null;
 };
 
 function setUp()
@@ -20,6 +21,7 @@ function setUp()
 		};
 	utils.include('operationHistory.js', namespace, 'Shift_JIS');
 	sv = namespace.window['piro.sakura.ne.jp'].operationHistory;
+	sv._tables = {};
 }
 
 function tearDown()
@@ -49,3 +51,83 @@ function test_getWindowById()
 	windowFromId = sv.getWindowById('does not exist!');
 	assert.isNull(windowFromId);
 }
+
+
+function registerEntries()
+{
+	var name = 'foobar';
+
+	// register global-anonymous
+	sv.addEntry({ label : 'anonymous 1' });
+	sv.addEntry({ label : 'anonymous 2' });
+	sv.addEntry({ label : 'anonymous 3' });
+
+	// register global-named
+	sv.addEntry(name, { label : 'named 1' });
+	sv.addEntry(name, { label : 'named 2' });
+	sv.addEntry(name, { label : 'named 3' });
+
+	// register window-anonymous
+	sv.addEntry({ label : 'window, anonymous 1' }, win);
+	sv.addEntry({ label : 'window, anonymous 2' }, win);
+	sv.addEntry({ label : 'window, anonymous 3' }, win);
+
+	// register window-named
+	sv.addEntry(name, { label : 'window, named 1' }, win);
+	sv.addEntry(name, { label : 'window, named 2' }, win);
+	sv.addEntry(name, { label : 'window, named 3' }, win);
+
+}
+
+test_addEntry.setUp = function() {
+	yield Do(windowSetUp());
+	registerEntries();
+};
+test_addEntry.tearDown = windowTearDown;
+function test_addEntry()
+{
+	var name = 'foobar';
+
+	function assertHistory(aItems, aCurrentIndex, aHistory)
+	{
+		assert.equals(aItems.length, aHistory.entries.length);
+		assert.equals(aItems,
+		              aHistory.entries.map(function(aEntry) {
+		                return aEntry.label;
+		              }));
+		assert.equals(aCurrentIndex, aHistory.index);
+	}
+
+	assertHistory(
+		['anonymous 1',
+		 'anonymous 2',
+		 'anonymous 3'],
+		2,
+		sv.getHistory()
+	);
+
+	assertHistory(
+		['named 1',
+		 'named 2',
+		 'named 3'],
+		2,
+		sv.getHistory(name)
+	);
+
+	assertHistory(
+		['window, anonymous 1',
+		 'window, anonymous 2',
+		 'window, anonymous 3'],
+		2,
+		sv.getHistory(win)
+	);
+
+	assertHistory(
+		['window, named 1',
+		 'window, named 2',
+		 'window, named 3'],
+		2,
+		sv.getHistory(name, win)
+	);
+}
+
