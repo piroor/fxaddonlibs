f0cc6b90c6710ddd4a1540646a5c6e2c03340388
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2010-01-04 06:01:28 +0000

    履歴ライブラリ
    git-svn-id: https://www.cozmixng.org/repos/piro/fx3-compatibility-lib/trunk@5699 599a83e7-65a4-db11-8015-0010dcdd6dc2

---
+++ operationHistory.js
@@ -0,0 +1,354 @@
+/*
+ UI Operations Global Undo History Manager
+
+ Usage:
+   var OH = window['piro.sakura.ne.jp'].operationHistory;
+
+   // window specific history
+   OH.addEntry(
+     'MyAddonFeature',
+     { label  : 'Change tabbar position',
+       onUndo : function() { ... },
+       onRedo : function() { ... } },
+     window
+   );
+   OH.undo('MyAddonFeature', window);
+   OH.redo('MyAddonFeature', window);
+
+   // global history (not associated to window)
+   OH.addEntry(
+     'MyAddonFeature',
+     { ... }
+   );
+   OH.undo('MyAddonFeature');
+
+   // anonymous, window specific
+   OH.addEntry({ ... }, window);
+   OH.undo(window);
+
+   // anonymous, global
+   OH.addEntry({ ... });
+   OH.undo();
+
+   // When you want to use "window" object in the global history,
+   // you should use the ID string instead of the "window" object
+   // to reduce memory leak. For example...
+   OH.addEntry({
+     id : OH.getWindowId(targetWindow),
+     onUndo : function() {
+       var w = OH.getWindowById(this.id);
+       w.MyAddonService.undoSomething();
+     },
+     onRedo : ...
+   });
+
+   // enumerate history entries
+   var history = OH.getHistory('MyAddonFeature', window); // options are same to undo/redo
+   OH.entries.forEach(function(aEntry, aIndex) {
+     var item = MyService.appendItem(aEntry.label);
+     if (aIndex == history.index)
+       item.setAttribute('checked', true);
+   });
+
+ lisence: The MIT License, Copyright (c) 2009-2010 SHIMODA "Piro" Hiroshi
+   http://www.cozmixng.org/repos/piro/fx3-compatibility-lib/trunk/license.txt
+ original:
+   http://www.cozmixng.org/repos/piro/fx3-compatibility-lib/trunk/operationHistory.js
+*/
+(function() {
+	const currentRevision = 3;
+
+	if (!('piro.sakura.ne.jp' in window)) window['piro.sakura.ne.jp'] = {};
+
+	var loadedRevision = 'operationHistory' in window['piro.sakura.ne.jp'] ?
+			window['piro.sakura.ne.jp'].operationHistory.revision :
+			0 ;
+	if (loadedRevision && loadedRevision > currentRevision) {
+		return;
+	}
+
+	var tables = {};
+	if (loadedRevision) {
+		tables = window['piro.sakura.ne.jp'].operationHistory._tables;
+		window['piro.sakura.ne.jp'].destroy();
+	}
+
+	var Cc = Components.classes;
+	var Ci = Components.interfaces;
+
+	window['piro.sakura.ne.jp'].operationHistory = {
+		revision : currentRevision,
+
+		kMAX_ENTRIES : 999,
+		kWINDOW_ID : 'ui-operation-global-history-window-id',
+
+		addEntry : function()
+		{
+			if (this._doingUndo)
+				return;
+
+			var options = this._getOptionsFromArguments(arguments);
+
+			var history = options.history;
+			var entries = history.entries;
+
+			entries = entries.slice(0, history.index+1);
+			entries.push(options.data);
+			entries = entries.slice(-this.kMAX_ENTRIES);
+
+			history.entries = entries;
+			history.index = entries.length-1;
+		},
+
+		getHistory : function()
+		{
+			var options = this._getOptionsFromArguments(arguments);
+			return options.history;
+		},
+
+		undo : function()
+		{
+			var options = this._getOptionsFromArguments(arguments);
+			var history = options.history;
+			if (history.index < 0)
+				return false;
+
+			this._doingUndo = true;
+			var processed = false;
+			while (processed === false)
+			{
+				let error;
+				let data = history.entries[history.index--];
+				let f = this._getAvailableFunction(data.onUndo, data.onundo, data.undo);
+				let done = false;
+				try {
+					if (f) {
+						processed = f.call(data);
+						done = true;
+					}
+					else {
+						processed = true;
+					}
+				}
+				catch(e) {
+					error = e;
+				}
+				this._dispatchEvent('UIOperationGlobalHistoryUndo', options, data, done);
+			}
+			this._doingUndo = false;
+
+			if (error) throw error;
+			return true;
+		},
+
+		redo : function()
+		{
+			var options = this._getOptionsFromArguments(arguments);
+			var history = options.history;
+			if (history.index > history.entries.length-1)
+				return false;
+
+			this._doingUndo = true;
+			var processed = false;
+			while (processed === false)
+			{
+				let error;
+				let f = this._getAvailableFunction(data.onRedo, data.onredo, data.redo);
+				let done = false;
+				try {
+					if (f) {
+						processed = f.call(data);
+						done = true;
+					}
+					else {
+						processed = true;
+					}
+				}
+				catch(e) {
+					error = e;
+				}
+				this._dispatchEvent('UIOperationGlobalHistoryRedo', options, data, done);
+			}
+			this._doingUndo = false;
+
+			if (error) throw error;
+			return true;
+		},
+
+		getWindowId : function(aWindow)
+		{
+			var windowId;
+			try {
+				windowId = aWindow.document.documentElement.getAttribute(this.kWINDOW_ID) ||
+							this.SessionStore.getWindowValue(aWindow, this.kWINDOW_ID);
+			}
+			catch(e) {
+			}
+			if (!windowId) {
+				windowId = 'window-'+Date.now()+parseInt(Math.random() * 65000);
+				aWindow.document.documentElement.setAttribute(this.kWINDOW_ID, windowId);
+				try {
+					this.SessionStore.setWindowValue(aWindow, this.kWINDOW_ID, windowId);
+				}
+				catch(e) {
+				}
+			}
+			return windowId;
+		},
+
+		getWindowById : function(aId)
+		{
+			var targets = this.WindowMediator.getZOrderDOMWindowEnumerator('*', true);
+			while (targets.hasMoreElements())
+			{
+				let target = targets.getNext().QueryInterface(Ci.nsIDOMWindowInternal);
+				if (aId == this.getWindowId(target))
+					return target;
+			}
+			return null;
+		},
+
+
+		/* PRIVATE METHODS */
+
+		_doingUndo : false,
+		_tables : tables,
+
+		initialized : false,
+
+		init : function()
+		{
+			var targets = this.WindowMediator.getZOrderDOMWindowEnumerator('navigator:browser', true);
+			while (targets.hasMoreElements())
+			{
+				let target = targets.getNext().QueryInterface(Ci.nsIDOMWindowInternal);
+				if (
+					'piro.sakura.ne.jp' in target &&
+					'operationHistory' in target['piro.sakura.ne.jp'] &&
+					target['piro.sakura.ne.jp'].operationHistory.initialized
+					) {
+					this._tables = target['piro.sakura.ne.jp'].operationHistory._tables;
+					break;
+				}
+			}
+
+			window.addEventListener('unload', this, false);
+
+			this.initialized = true;
+		},
+
+		destroy : function()
+		{
+			window.removeEventListener('unload', this, false);
+		},
+
+		_dispatchEvent : function(aType, aOptions, aData, aDone)
+		{
+			var d = aOptions.window ? aOptions.window.document : document ;
+			var event = d.createEvent('Events');
+			event.initEvent(aType, true, false);
+			event.name = aOptions.name;
+			event.data = aData;
+			event.done = aDone;
+			d.dispatchEvent(event);
+		},
+
+		_getOptionsFromArguments : function(aArguments)
+		{
+			var w = null, name, data = null;
+			Array.slice(aArguments).some(function(aArg) {
+				if (aArg instanceof Ci.nsIDOMWindow)
+					w = aArg;
+				else if (typeof aArg == 'string')
+					name = aArg;
+				else if (aArg)
+					data = aArg;
+
+				return (w && name && data);
+			});
+
+			if (!name)
+				name = w ? 'window' : 'global' ;
+
+			var tableName = encodeURIComponent(name);
+
+			var windowId = w ? this.getWindowId(window) : null ;
+			if (windowId)
+				tableName += '::'+windowId;
+
+			if (!(tableName in this._tables)) {
+				this._tables[tableName] = {
+					entries  : [],
+					index    : -1,
+					windowId : windowId
+				};
+			}
+
+			return {
+				name     : name,
+				window   : w,
+				windowId : windowId,
+				data     : data,
+				history  : this._tables[tableName]
+			};
+		},
+
+		_getAvailableFunction : function()
+		{
+			var functions = Array.slice(arguments);
+			for (var i in functions)
+			{
+				let f = functions[i];
+				if (f && typeof f == 'function')
+					return f;
+			}
+			return null;
+		},
+
+		_deleteWindowTables : function()
+		{
+			var id = this.getWindowId(window);
+			if (!id) return;
+
+			var removedTables = [];
+			for (let i in this._tables)
+			{
+				if (id == this._tables[i].windowId)
+					removedTables.push(i);
+			}
+			removedTables.forEach(function(aName) {
+				delete this._tables[aName];
+			}, this);
+		},
+
+		get WindowMediator() {
+			if (!this._WindowMediator) {
+				this._WindowMediator = Cc['@mozilla.org/appshell/window-mediator;1'].getService(Ci.nsIWindowMediator);
+			}
+			return this._WindowMediator;
+		},
+		_WindowMediator : null,
+
+		get SessionStore() { 
+			if (!this._SessionStore) {
+				this._SessionStore = Cc['@mozilla.org/browser/sessionstore;1'].getService(Ci.nsISessionStore);
+			}
+			return this._SessionStore;
+		},
+		_SessionStore : null,
+
+		handleEvent : function(aEvent)
+		{
+			switch (aEvent.type)
+			{
+				case 'unload':
+					this._deleteWindowTables();
+					this.destroy();
+					return;
+			}
+		}
+
+	};
+
+	window['piro.sakura.ne.jp'].operationHistory.init();
+})();
