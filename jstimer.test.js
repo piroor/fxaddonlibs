902b75f9bcfdcd2b87e980fd0a2acd42aa697bb3
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2010-06-28 16:03:08 +0000

    ウィンドウが閉じられたら自動的にタイマーを停止する
    git-svn-id: https://www.cozmixng.org/repos/piro/fx3-compatibility-lib/trunk@6780 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- jstimer.test.js
+++ jstimer.test.js
@@ -12,7 +12,6 @@ function tearDown()
 	{
 		namespace.Timer.cancel(id);
 	}
-};
 }
 
 function test_setTimeout()
@@ -54,6 +53,38 @@ function test_clearTimeout()
 	assert.equals(0, count);
 }
 
+test_autoClearTimeout.setUp = function() {
+	yield utils.setUpTestWindow();
+}
+test_autoClearTimeout.tearDown = function() {
+	utils.tearDownTestWindow();
+}
+function test_autoClearTimeout()
+{
+	var win = utils.getTestWindow();
+	var counter = { value : 0 };
+	win.test_autoClearIntervalCount = counter;
+	win.setTimeout(<![CDATA[
+		window.test_autoClearTimeout = function(aValue) {
+			window.test_autoClearTimeoutCount.value += aValue;
+		};
+	]]>.toString(), 0);
+	yield 300;
+	assert.isFunction(win.test_autoClearTimeout);
+
+	var id = namespace.setTimeout(
+			win.test_autoClearTimeout,
+			300,
+			1
+		);
+	assert.equals(0, counter.value);
+	utils.tearDownTestWindow();
+	yield 400;
+
+	assert.isTrue(win.closed);
+	assert.equals(0, counter.value);
+}
+
 function test_setInterval()
 {
 	assert.isFunction(namespace.setInterval);
@@ -89,4 +120,36 @@ function test_clearInterval()
 	namespace.clearInterval(id);
 	yield 500;
 	assert.equals(0, count);
+}
+
+test_autoClearInterval.setUp = function() {
+	yield utils.setUpTestWindow();
+}
+test_autoClearInterval.tearDown = function() {
+	utils.tearDownTestWindow();
+}
+function test_autoClearInterval()
+{
+	var win = utils.getTestWindow();
+	var counter = { value : 0 };
+	win.test_autoClearIntervalCount = counter;
+	win.setTimeout(<![CDATA[
+		window.test_autoClearInterval = function(aValue) {
+			window.test_autoClearIntervalCount.value += aValue;
+		};
+	]]>.toString(), 0);
+	yield 300;
+	assert.isFunction(win.test_autoClearInterval);
+
+	var id = namespace.setInterval(
+			win.test_autoClearInterval,
+			200,
+			1
+		);
+	assert.equals(0, counter.value);
+	utils.tearDownTestWindow();
+	yield 400;
+
+	assert.isTrue(win.closed);
+	assert.equals(0, counter.value);
 }
